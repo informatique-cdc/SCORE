4ce0dc6a3ab3965d0d5a64db3ed376663cc5991d
Pierre Houdyer <pierre.houdyer@caissedesdepots.fr>
2026-04-08 13:13:27 +0200

    style: format merge migration file

--- connectors/migrations/0004_merge_20260408_1308.py
+++ connectors/migrations/0004_merge_20260408_1308.py
@@ -4,11 +4,9 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('connectors', '0003_add_elasticsearch_connector_type'),
-        ('connectors', '0003_add_encrypted_secret'),
+        ("connectors", "0003_add_elasticsearch_connector_type"),
+        ("connectors", "0003_add_encrypted_secret"),
     ]
 
-    operations = [
-    ]
+    operations = []
